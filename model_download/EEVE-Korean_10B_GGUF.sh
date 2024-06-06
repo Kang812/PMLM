24f29b735e50a0fc1372fb4ac27d7347355b6140
Kang812 <bgw2001@gmail.com>
2024-06-06 15:35:45 +0000

    Add model download sh file

---
+++ model_download/EEVE-Korean_10B_GGUF.sh
@@ -0,0 +1,5 @@
+huggingface-cli download \
+    heegyu/EEVE-Korean-Instruct-10.8B-v1.0-GGUF \
+    ggml-model-Q5_K_M.gguf \
+    --local-dir /media/kwbyun/new_volume/pmlm_model \
+    --local-dir-use-symlinks False
